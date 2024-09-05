f37a639ee09906f2ac463d4cea863957a5f6e6ad
Faisal Achmad Dwi Cahyono <faisalachmaddwic@gmail.com>
2024-09-05 16:04:22 +0700

    INIT Dashboard

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ URL::asset('/images/kopnus.jpg') }}" type="image/x-icon">
 
     @vite('resources/js/app.js')
     @inertiaHead
